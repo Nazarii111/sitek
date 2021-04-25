7155fc3d12c57cb7fa57e9daf2dfab9151135f3b
Nazarii111 <83161312+Nazarii111@users.noreply.github.com>
2021-04-25 12:25:12 +0300

    edfefe

---
+++ 2.js
@@ -0,0 +1,23 @@
+img1.onclick = function memugd(){ 
+   if(img1.className = "blok-img-3d1"){
+      img1.className +="blok-img-3d1-active"
+   } 
+}
+
+img2.onclick = function memug(){
+   if(img2.className = "blok-img-3d"){
+         img1.className = "blok-img-3d1"
+   } 
+}
+var img1 = document.getElementById('car');
+img1.onclick = function memugd(){ 
+   if(img1.className = "blok-img-3d1"){
+      img1.className +="blok-img-3d1-active"
+   } 
+}
+var img2 = document.getElementById('car1');
+img2.onclick = function memug(){
+   if(img2.className = "blok-img-3d"){
+      img1.className = "blok-img-3d1"
+   } 
+}
